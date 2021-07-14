88ee60bd3c5c04ffb887db6785fdaef78c2659e1
Xiangrui Meng <harrymengpku@outlook.com>
2021-07-14 18:59:34 +0100

    minor revision on primary analysis script: mr-mega pc number.

--- scripts/S1.primary.analysis.sh
+++ scripts/S1.primary.analysis.sh
@@ -6,7 +6,7 @@
 #$ -N primary.analysis
 
 WD="/SAN/ugi/mdd/trans_ethnic_ma"
-/SAN/ugi/mdd/MR-MEGA/MR-MEGA -i $WD/scripts/mr_mega.in.txt -o $WD/results/AFR.EAS.SAS.HIS.MDD3SUM.MRMEGA
+/SAN/ugi/mdd/MR-MEGA/MR-MEGA -i $WD/scripts/mr_mega.in.txt --pc 5 -o $WD/results/AFR.EAS.SAS.HIS.MDD3SUM.MRMEGA
 R --vanilla --args $WD/scripts/mr_mega.in.txt $WD/data/RE2C.in.txt < $WC/scripts/RE2C_data_prepare.R
 bash /SAN/ugi/mdd/RE2C/RE2C.bash --input $WD/data/RE2C.in.txt  --output $WD/results/RE2C.out
 
